f41967de7e03db0fe988a7ac3e96914a702a2acb
HyungtaeMoon <blessmht@gmail.com>
2018-10-08 20:00:49 +0900

    [ADD] settings 패키지화, SECRET_KEY json으로 분리

---
+++ app/config/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
