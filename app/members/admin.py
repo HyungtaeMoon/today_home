b4181a7cee782033968b1408207f460b1efebd28
HyungtaeMoon <blessmht@gmail.com>
2018-10-15 18:55:08 +0900

    [ADD] User에 profile_img 추가, S3와 연결 확인

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+
 from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
-    pass
+    fieldsets = (
+        (None, {
+            'fields':(
+                'username',
+                'password',
+            ),
+        }),
+        ('개인정보', {
+            'fields':(
+                'profile_img',
+            ),
+        }),
+    )
 
 
 admin.site.register(User, UserAdmin)
--- app/members/models.py
+++ app/members/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    profile_img = models.ImageField(upload_to='user', blank=True)
