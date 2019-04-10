3f41eed1a8096f547ea156d08788fedb1ff1e69a
HyungtaeMoon <blessmht@gmail.com>
2019-04-10 18:47:26 +0900

    [ADD] 게시판 메인 페이지 기능, 템플릿 구현

--- app/community/urls.py
+++ app/community/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'community'
 
 urlpatterns = [
-    path(''),
+    path('', views.community_notice_list, name='notice'),
 ]
--- app/community/views.py
+++ app/community/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Community
+
+
+def community_notice_list(request):
+    community_list = Community.objects.all()
+    context = {
+        'community_list': community_list,
+    }
+    return render(request, 'community/main_notice.html', context)
