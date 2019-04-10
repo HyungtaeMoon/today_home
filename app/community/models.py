19955850b6f8a01730c7f278ad7e453a4f6fc2c1
HyungtaeMoon <blessmht@gmail.com>
2019-04-10 17:47:25 +0900

    [ADD] community 의 메인 모델 생성

--- app/community/models.py
+++ app/community/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Community(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='community')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
