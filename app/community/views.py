2b6df18150279a237ad358e34e6b0b03dcbf143f
HyungtaeMoon <blessmht@gmail.com>
2019-04-11 16:51:37 +0900

    [FIX] 게시판 main 페이지에 Search 버튼 등 템플릿 개선

--- app/community/views.py
+++ app/community/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 from .models import Community, CommentCommunity
 
+
+User = get_user_model()
 
 def community_notice_list(request):
     community_list = Community.objects.all()
@@ -19,3 +22,10 @@ def community_detail(request, community_pk):
         'comment_list': comment_list,
     }
     return render(request, 'community/community_detail.html', context)
+
+
+# def community_create(request):
+#     if request.method == 'POST':
+#         user = User.objects.get(pk=request.user.id)
+#         if user is not None:
+#             pass
