95def39567832f440cdd1228128e9385169e7abf
HyungtaeMoon <blessmht@gmail.com>
2019-04-20 19:21:52 +0900

    [FIX] 커뮤니티 앱 모델명 변경
    1. Community 에서 Question 으로 변경
    2. CommentCommunity 에서 CommentQuestion 으로 변경

--- app/community/models.py
+++ app/community/models.py
@@ -4,26 +4,38 @@ from django.db import models
 User = get_user_model()
 
 
-class Community(models.Model):
+# 모델은 단수로 사용
+class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to='community')
+    image = models.ImageField(upload_to='question', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = '질문게시판'
+        verbose_name_plural = f'{verbose_name} 목록'
+        ordering = ['-pk']
 
     def __str__(self):
         return self.title
 
 
-class CommentCommunity(models.Model):
+# 모델은 단수로 사용하고, 주기능을 앞에 두어 Camel-case 로 설정
+class CommentQuestion(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to='community', null=True)
+    image = models.ImageField(upload_to='question_comment', null=True)
     like = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = '질문게시판 댓글'
+        verbose_name_plural = f'{verbose_name} 목록'
+        ordering = ['-pk']
 
     def __str__(self):
         return self.content
